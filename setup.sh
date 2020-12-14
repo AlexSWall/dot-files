8855fb168c8a213c3684ced8af4f5e917965ce0a
Alex W <alexander.stephen.wall@gmail.com>
2020-12-14 15:31:23 +0000

    Improved setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,53 @@
 #!/bin/bash
 
 PWD="$(pwd)"
+OLD="$(pwd)/old"
+mkdir -p "$OLD"
 
-# Home directory dotfiles
-rm -rf "$HOME/.bashrc"
-ln -s "$PWD/.bashrc" "$HOME/.bashrc"
-rm -rf "$HOME/.vimrc"
-ln -s "$PWD/.vimrc" "$HOME/.vimrc"
-rm -rf "$HOME/.tmux.conf"
-ln -s "$PWD/.tmux.conf" "$HOME/.tmux.conf"
-rm -rf "$HOME/.gitconfig"
-ln -s "$PWD/.gitconfig" "$HOME/.gitconfig"
+# == Home directory dotfiles ==
+for f in ".bashrc .vimrc .tmux.conf .gitconfig"
+do
+	if [ -e "$HOME/$f" ]; then
+		mv "$HOME/$f $OLD/$f"
+	fi
+	ln -s "$PWD/$f" "$HOME/$f"
+done
 
-# Vim Monokai
+# == Monokai ==
 mkdir -p "$HOME/.vim/colors"
-rm -rf "$HOME/.vim/colors/monokai.vim"
+if [ -e "$HOME/.vim/colors/monokai.vim" ]; then
+	mv "$HOME/.vim/colors/monokai.vim $OLD/monokai.vim"
+fi
 ln -s "$PWD/monokai.vim" "$HOME/.vim/colors/monokai.vim"
 
-# Nvim
+# == Neovim ==
 mkdir -p "$HOME/.config/nvim"
-rm -rf "$HOME/.config/nvim/init.vim"
+# init.vim
+if [ -e "$HOME/.config/nvim/init.vim" ]; then
+	mv "$HOME/config/nvim/init.vim" "$OLD/init.vim"
+fi
 ln -s "$PWD/init.vim" "$HOME/.config/nvim/init.vim"
-rm -rf "$HOME/.config/nvim/coc-settings.json"
+# coc-settings.json
+if [ -e "$HOME/.config/nvim/coc-settings.json" ]; then
+	mv "$HOME/config/nvim/coc-settings.json" "$OLD/coc-settings.json"
+fi
 ln -s "$PWD/coc-settings.json" "$HOME/.config/nvim/coc-settings.json"
+
+# == Neovim plugins ==
+# mkdir -p "$HOME/.local/share/nvim/site"
+
+# if [ -e "$HOME/.local/share/nvim/site/autoload/plug.vim" ]; then
+# 	ln -s "$HOME/.vim/autoload" "$HOME/.local/share/nvim/site/autoload"
+# fi
+
+if [ ! -e "$HOME/.vim/autoload/plug.vim" ]; then
+	curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
+		 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	echo "Install nodejs, npm, and yarn for neovim"
+fi
+
+# == Tmux plugins ==
+
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
