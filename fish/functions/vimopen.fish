8fe7f8f1a996df84efb2fe3c31740bb0418dbcb7
Alex W <alexander.stephen.wall@gmail.com>
2022-02-13 16:37:59 +0000

    Adding vimopen to fish functions

---
+++ fish/functions/vimopen.fish
@@ -0,0 +1,44 @@
+function vimopen
+
+	if test (count $argv) = 1
+		set DIRECTORY '.'
+		set REGEX_OR_GLOB $argv[1]
+
+	else if test (count $argv) = 2
+		set DIRECTORY $argv[1]
+		set REGEX_OR_GLOB $argv[2]
+
+	else
+		echo 'Usage: vimdiff [directory] regex-or-glob-string'
+		echo 'Number of arguments given:' (count $argv)
+		return 1
+	end
+
+	# Check whether interpreting as a regex gives results...
+	set FILES (find -E "$DIRECTORY" -regex "$REGEX_OR_GLOB" -type f 2>/dev/null)
+
+	if test ! -z "$FILES"
+		#echo 'Interpreting input as a regular expression.'
+
+	else
+		# Check whether interpreting as a regex with ".*/" prefixed gives
+		# results...
+		set FILES (find -E "$DIRECTORY" -regex ".*/$REGEX_OR_GLOB" -type f 2>/dev/null)
+
+		if test ! -z "$FILES"
+			#echo 'Interpreting input as a regular expression, adding a ".*/" prefix.'
+
+		else
+			# Regexes failed, assume it's meant to be a glob...
+
+			#echo 'Interpreting input as a glob.'
+			set FILES (find "$DIRECTORY" -name "$REGEX_OR_GLOB" -type f 2>/dev/null)
+		end
+	end
+
+	if test ! -z "$FILES"
+		vim (echo $FILES)
+	else
+		echo 'No matches'
+	end
+end
