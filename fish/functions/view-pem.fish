86adc4440b3c662061949109c545bf0843ff263f
Alex W <alexander.stephen.wall@gmail.com>
2022-05-04 03:07:49 +0100

    Generalizing view-pem to deal with certificate chains

--- fish/functions/view-pem.fish
+++ fish/functions/view-pem.fish
@@ -18,16 +18,31 @@ function view-pem
 		# leading dot, and replacing any slashes with underscores.
 		set outfile (string replace --all '/' '__' (string replace --regex '^\.' '_' $file))
 
-		# Save the output of the openssl command for this file to its temporary
-		# file.
-		openssl x509 in "$file" -inform pem -noout -text > "$dir/$outfile"
+		# Find all of the instances of CERTIFICATE-----.
+		# These come in pairs; the first of each pair will be START and the second
+		# will be END.
+		# Treat the certificates separately and concatenate the outputs together.
+		set positions (grep -n -- ' CERTIFICATE-----' $file | awk -F: '{print $1}')
+		for i in (seq (math (count $positions) / 2))
+			# This command extracts the certificate from the $i-th BEGIN
+			# CERTIFICATE to the $i-th END CERTIFICATE (using tail and head) and
+			# passes this to an openssl command to be parsed and have the output
+			# saved to the temporary output file.
+			set start $positions[(math 2 x $i - 1 )]
+			set end   $positions[(math 2 x $i     )]
+			openssl x509 \
+				-in (tail -n +$start $file | head -n (echo (math $end - $start + 1)) | psub) \
+				-inform pem \
+				-noout \
+				-text >> "$dir/$outfile"
+		end
 
 		# Accumulate all of the temporary output files.
 		set -a outfiles "$dir/$outfile"
 	end
 
 	# View the openssl outputs.
-	vimdiff $outfiles
+	vim $outfiles
 
 	# Tidy up the temporary files.
 	rm -r $dir
