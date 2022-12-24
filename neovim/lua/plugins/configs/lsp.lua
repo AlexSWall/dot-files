bf9473e6c8165d01bec90901d3d97b688faf93a7
Alex W <alexander.stephen.wall@gmail.com>
2022-12-24 05:34:39 +0000

    Added nerd font Neovim LSP sign column diagnostic icons.

--- neovim/lua/plugins/configs/lsp.lua
+++ neovim/lua/plugins/configs/lsp.lua
@@ -119,6 +119,12 @@ function M.setup()
 
 		require('lspconfig')[server].setup(opts)
 	end
+
+	local signs = { Error = '', Warn = '', Info = '', Hint = '' }
+	for type, icon in pairs(signs) do
+		local hl = 'DiagnosticSign' .. type
+		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	end
 end
 
 return M
