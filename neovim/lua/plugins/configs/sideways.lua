464f8f1625930ea2905115f86fe89d15f1386e88
Alex W <alexander.stephen.wall@gmail.com>
2022-12-31 04:11:12 +0000

    Fixed incorrect Sideways keymapping

--- neovim/lua/plugins/configs/sideways.lua
+++ neovim/lua/plugins/configs/sideways.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.setup()
 	local nmap = require('utils.keymap').nmap
 
-	nmap('<cmd>SidewaysLeft<CR>', '<Leader><Leader>h', 'Move to previous function argument')
+	nmap('<Leader><Leader>h', '<cmd>SidewaysLeft<CR>', 'Move to previous function argument')
 	nmap('<Leader><Leader>l', '<cmd>SidewaysRight<CR>', 'Move to next function argument')
 end
 
