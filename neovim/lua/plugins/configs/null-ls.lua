404d67f8104dec72e7634d4be3a319890f6732ef
Alex W <alexander.stephen.wall@gmail.com>
2023-01-07 06:18:12 +0000

    Adding addition neovim python ruff null_ls CLI arguments

--- neovim/lua/plugins/configs/null-ls.lua
+++ neovim/lua/plugins/configs/null-ls.lua
@@ -7,17 +7,29 @@ function M.setup()
 	local formatting   = require('null-ls').builtins.formatting
 
 	local ruff_extra_args = {
-		-- Removed: McCabe (C90), flake8-print (T20), flake8-errmsg (EM), eradicate (ERA), flake8-pie (PIE)
-		'--select', 'F,E,W,I,D,UP,N,YTT,ANN,S,BLE,FBT,B,A,C4,T10,ISC,ICN,PT,Q,RET,SIM,TID,ARG,DTZ,PD,PGH,PLC,PLE,PLR,PLW,RUF',
+		-- Removed:
+		-- - McCabe (C90),
+		-- - flake8-blind-except (BLE),
+		-- - flake8-print (T20),
+		-- - flake8-errmsg (EM),
+		-- - eradicate (ERA),
+		-- - flake8-pie (PIE)
+		'--select', 'F,E,W,I,D,UP,N,YTT,ANN,S,FBT,B,A,C4,T10,ISC,ICN,PT,Q,RET,SIM,TID,ARG,DTZ,PD,PGH,PLC,PLE,PLR,PLW,RUF',
 		'--ignore', ''
 		-- These are covered by pyright.
 		.. 'F401,F821,F841,'
-		-- Don't force one-line docstrings.
-		.. 'D200,'
+		-- 'Temporarily allow no docstrings.'
+		-- .. 'D100,D101,D102,D103,D104,D105,D106,D107,'
 		-- No blank lines before class docstring.
 		.. 'D203,'
+		-- Don't force one-line docstrings.
+		.. 'D200,'
 		-- Multiline docstrings should start on next line.
 		.. 'D212,'
+		-- Allow dynamically-typed expressions via typing.Any.
+		.. 'ANN401,'
+		-- Allow unittest.assertEquals (etc.)
+		.. 'PT009,'
 		-- Allow single quotes.
 		.. 'Q000,Q001'
 		.. '',
