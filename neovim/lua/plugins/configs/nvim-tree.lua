2ef063ceaaad012078ee98975269bc3f32b4fb0c
Alex W <alexander.stephen.wall@gmail.com>
2023-05-11 21:19:58 +0100

    Updating nvim-tree configuration due to .resize() breaking change.

--- neovim/lua/plugins/configs/nvim-tree.lua
+++ neovim/lua/plugins/configs/nvim-tree.lua
@@ -16,8 +16,8 @@ function M.setup()
 	local nmap = require('utils.keymap').nmap
 	nmap('<Leader>nf', require('nvim-tree.api').tree.toggle, 'Toggle nvim-tree')
 	nmap('<Leader>nv', function() require('nvim-tree.api').tree.find_file(vim.api.nvim_buf_get_name(0)) end, 'Go to current file in nvim-tree')
-	nmap('<Leader>nh', function() require('nvim-tree').resize('-10') end, 'Reduce nvim-tree width')
-	nmap('<Leader>nl', function() require('nvim-tree').resize('+10') end, 'Increase nvim-tree width')
+	nmap('<Leader>nh', function() require('nvim-tree.view').resize('-10') end, 'Reduce nvim-tree width')
+	nmap('<Leader>nl', function() require('nvim-tree.view').resize('+10') end, 'Increase nvim-tree width')
 	nmap('<Leader>nc', require('nvim-tree.api').tree.collapse_all, 'Collapse all nvim-tree folders')
 	nmap('<Leader>na', function() require('nvim-tree.api').tree.collapse_all(true) end,
 	     'Collapse all nvim-tree folders except current file\'s hierarchy')
