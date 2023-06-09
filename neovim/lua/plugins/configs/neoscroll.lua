d648b9d66d38d52d35d933c33d4b49a5a04c8d4b
Alex W <alexander.stephen.wall@gmail.com>
2023-06-09 19:33:23 +0100

    Changing neoscroll to only be used when Tmux is not being used.

--- neovim/lua/plugins/configs/neoscroll.lua
+++ neovim/lua/plugins/configs/neoscroll.lua
@@ -1,20 +1,24 @@
 local M = {}
 
 function M.setup()
-	require('neoscroll').setup({
-		-- easing_function = 'quadratic'
-		mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>', 'zt', 'zz', 'zb'},
-	})
+	local is_in_tmux = (os.getenv('TMUX') ~= nil)
 
-	require('neoscroll.config').set_mappings({
-		['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '70'}},
-		['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '70'}},
-		['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '140'}},
-		['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '140'}},
-		['zt']    = {'zt', {'70'}},
-		['zz']    = {'zz', {'70'}},
-		['zb']    = {'zb', {'70'}}
-	})
+	if not is_in_tmux then
+		require('neoscroll').setup({
+			-- easing_function = 'quadratic'
+			mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>', 'zt', 'zz', 'zb'},
+		})
+
+		require('neoscroll.config').set_mappings({
+			['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '35'}},
+			['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '35'}},
+			['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '70'}},
+			['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '70'}},
+			['zt']    = {'zt', {'35'}},
+			['zz']    = {'zz', {'35'}},
+			['zb']    = {'zb', {'35'}}
+		})
+	end
 end
 
 return M
