63819e9db515f0c31c8c0d6bc34306c67ed90147
Alex W <alexander.stephen.wall@gmail.com>
2023-06-23 13:45:20 +0100

    Enabling neoscroll regardless of Tmux

--- neovim/lua/plugins/configs/neoscroll.lua
+++ neovim/lua/plugins/configs/neoscroll.lua
@@ -1,24 +1,20 @@
 local M = {}
 
 function M.setup()
-	local is_in_tmux = (os.getenv('TMUX') ~= nil)
+	require('neoscroll').setup({
+		-- easing_function = 'quadratic'
+		mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>', 'zt', 'zz', 'zb'},
+	})
 
-	if not is_in_tmux then
-		require('neoscroll').setup({
-			-- easing_function = 'quadratic'
-			mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>', 'zt', 'zz', 'zb'},
-		})
-
-		require('neoscroll.config').set_mappings({
-			['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '35'}},
-			['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '35'}},
-			['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '70'}},
-			['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '70'}},
-			['zt']    = {'zt', {'35'}},
-			['zz']    = {'zz', {'35'}},
-			['zb']    = {'zb', {'35'}}
-		})
-	end
+	require('neoscroll.config').set_mappings({
+		['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '50'}},
+		['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '50'}},
+		['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '80'}},
+		['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '80'}},
+		['zt']    = {'zt', {'50'}},
+		['zz']    = {'zz', {'50'}},
+		['zb']    = {'zb', {'50'}}
+	})
 end
 
 return M
