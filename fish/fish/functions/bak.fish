04b6d5d69d8c82140b68bdd9d1ab2f6739214c80
Alex W <alexander.stephen.wall@gmail.com>
2023-08-21 22:41:33 +0100

    Transcending bak and unbak.

--- fish/fish/functions/bak.fish
+++ fish/fish/functions/bak.fish
@@ -2,34 +2,69 @@ function bak --description 'Backs up a file or directory.'
 
 	# == Begin Validation ==
 
-	function usage
+	function usage -a error_msg
+		if test -n "$error_msg"
+			printf "Error: $error_msg.\n\n"
+		end
 		echo 'Back up a file or directory.'
 		echo ''
 		echo 'Usage:'
-		echo '    bak [-m/--mv] <file/directory path to create a backup for>'
+		echo ''
+		echo '    bak [-m/--mv] <path> [optional-postfix]'
 		echo '    bak -h/--help'
 		echo ''
+		echo 'Description:'
+		echo ''
+		echo '    Here, a backed-up file/directory is off the form'
+		echo '        <original-name>.bak[.<optional-postfix>]'
+		echo ''
+		echo '    Given the file/directory path, a backup is created by copying, or moving if'
+		echo '    --mv is given, the file/directory to the same filepath with .bak appended.'
+		echo ''
+		echo '    If a postfix argument has been given, .<optional-postfix> will also be'
+		echo '    appended.'
+		echo ''
+		echo '    If such a filepath already exists (and a numeric postfix was not provided),'
+		echo '    a dot and number is appended. The number used is the smallest positive'
+		echo '    number bigger than any existing backup (up to 99).'
+		echo ''
+		echo 'Options:'
+		echo ''
 		echo '    -h/--help'
-		echo '        Prints this usage string and exits.'
+		echo '        Print this usage string and exit.'
 		echo ''
 		echo '    -m/--mv'
-		echo '        Optionally, moves the file instead of copying it.'
+		echo '        Move the file instead of copying it.'
 	end
 
 	argparse --name 'bak' 'm/mv' 'h/help' -- $argv
-	or usage && return 1
+	or echo && usage && return 1
 
 	if set -q _flag_help
 		usage && return
 	end
 
-	if not test (count $argv) -eq 1
-		usage && return 1
+	if test (count $argv) -eq 0 -o (count $argv) -gt 2
+		usage 'Incorrect number of arguments provided' && return 1
 	end
 
-	set -l FILEPATH $argv[1]
+	set -l filepath $argv[1]  # Required
+	set -l postfix  $argv[2]  # Optional
+	set -e argv
+
+	if test -z "$filepath"
+		usage 'Filepath cannot be empty' && return 1
+	end
 
 	# == End Validation ==
+
+	# Ensure we strip trailing backslashes (if filepath is a directory).
+	if test (string split '' $filepath)[-1] = '/'
+		if not test -d $filepath
+			usage 'Filepath with trailing slash given, but directory not found' && return 1
+		end
+		set filepath (string trim --right --chars '/' $filepath)
+	end
 
 	# Ensure we don't overwrite an existing backup.
 	#
@@ -37,21 +72,36 @@ function bak --description 'Backs up a file or directory.'
 	# backup postfix by counting down from 100 until we find a postfix that
 	# exists, if any, and setting our new postfix to one above it.
 
-	set -l postfix ''  # Initially empty; only set if we have existing backups.
+	set -l backup_to "$filepath.bak"
+	if test -n "$postfix"
+		set backup_to "$filepath.bak.$postfix"
+	end
 
-	if test -e $FILEPATH.bak
-		for i in (seq 100 -1 1)
-			if test -e $FILEPATH.bak.$i
-				set postfix .(math $i + 1)
-				break
+	if string match -q -r '^\d\d?$' "$postfix" > /dev/null
+		# Our postfix given is a number; don't try to avoid a clash by appending a
+		# numeric postfix.
+	else
+		# Our postfix (if any) is not a number; ensure we avoid clashes.
+
+		if test -e "$backup_to"
+
+			set -l clash_avoidance_done false
+
+			for i in (seq 100 -1 1)
+				if test -e "$backup_to.$i"
+					set numeric_postfix (math $i + 1)
+					set backup_to "$backup_to.$numeric_postfix"
+					set clash_avoidance_done true
+					break
+				end
 			end
-		end
 
-		# Handle special case where $FILEPATH.bak was taken but no
-		# $FILEPATH.bak.$i were taken - this means we want to use the first
-		# $FILEPATH.bak.$i, so we set the prefix to '.1'.
-		if test -z $postfix
-			set postfix '.1'
+			# Handle special case where $filepath.bak was taken but no
+			# $filepath.bak.$i were taken - this means we want to use the first
+			# $filepath.bak.$i, so we set the prefix to '.1'.
+			if not $clash_avoidance_done
+				set backup_to "$backup_to.1"
+			end
 		end
 	end
 
@@ -61,5 +111,27 @@ function bak --description 'Backs up a file or directory.'
 		set cmd mv
 	end
 
-	command $cmd $FILEPATH $FILEPATH.bak$postfix
+	# If we'd be overwriting another file/directory, get user confirmation.
+	if test -e "$backup_to"
+
+		echo "This will overwrite '$backup_to' with '$filepath'."
+		echo ''
+		read -l -P "Are you sure? [y/N] " confirm
+
+		switch (string lower $confirm)
+			case y ye yes
+				# Continue.
+			case '*'
+				echo 'Cancelling...'
+				return 1
+		end
+
+		rm -rf "$backup_to"
+	end
+
+	# Finally perform the back-up.
+	$cmd "$filepath" "$backup_to"
 end
+
+complete -c bak -s m -l mv -d 'Move the file instead of copying it'
+complete -c bak -s h -l help -d 'Print the usage string and exit'
--- fish/fish/functions/unbak.fish
+++ fish/fish/functions/unbak.fish
@@ -2,55 +2,144 @@ function unbak --description 'Restores a file or directory.'
 
 	# == Begin Validation ==
 
-	function usage
-		echo 'Restores a backed-up file or directory.'
+	function usage -a error_msg
+		if test -n "$error_msg"
+			printf "Error: $error_msg.\n\n"
+		end
+		echo 'Restore a backed-up file or directory.'
 		echo ''
 		echo 'Usage:'
-		echo '    unbak <file/directory path to create a backup for> [backup number]'
+		echo ''
+		echo '    unbak <path> [backup-postfix]'
 		echo '    unbak -h/--help'
 		echo ''
+		echo 'Description:'
+		echo ''
+		echo '    Here, a backed-up file/directory is off the form'
+		echo '        <original-name>.bak[.<optional-postfix>]'
+		echo ''
+		echo '    If both a path and backup postfix are given, a backup of the form'
+		echo '        <path-given>.bak.<postfix>'
+		echo '    is found and used.'
+		echo '    As a special case, if <postfix> is 0, <path-given>.bak is used instead.'
+		echo ''
+		echo '    Else, if the path is in the format of a backup, the postfix is stripped to'
+		echo '    restore the file/directory to.'
+		echo ''
+		echo '    Else, the path is considered to be the result of a restore and the backup is'
+		echo '    deduced; the most recent backup (i.e. highest number) is used.'
+		echo ''
+		echo '    Confirmation will be obtained before overwriting if a file/directory already'
+		echo '    exists at the target restore filepath.'
+		echo ''
+		echo 'Options:'
+		echo ''
 		echo '    -h/--help'
-		echo '        Prints this usage string and exits.'
+		echo '        Print this usage string and exit.'
+		echo ''
 	end
 
-	argparse --name 'unbak' 'h/help' -- $argv
-	or usage && return 1
+	argparse --name 'bak' 'h/help' -- $argv
+	or echo && usage && return 1
 
 	if set -q _flag_help
 		usage && return
 	end
 
 	if test (count $argv) -eq 0 -o (count $argv) -gt 2
-		usage && return 1
+		usage 'Incorrect number of arguments provided' && return 1
 	end
 
-	set -l FILEPATH $argv[1]
-	set -l NUM $argv[2]
+	set -l filepath $argv[1]  # Required
+	set -l postfix  $argv[2]  # Optional
+	set -e argv
+
+	if test -z "$filepath"
+		usage 'Filepath cannot be empty' && return 1
+	end
 
 	# == End Validation ==
 
-	set -l postfix
-	if test -z $NUM
-		set postfix ''
-	else
-		set postfix .$NUM
+	# Ensure we strip trailing backslashes (if filepath is a directory).
+	if test (string split '' $filepath)[-1] = '/'
+		if not test -d $filepath
+			usage 'Filepath with trailing slash given, but directory not found' && return 1
+		end
+		set filepath (string trim --right --chars '/' $filepath)
 	end
 
-	if string match -q -r ".*\.bak\.\d\d?" $FILEPATH > /dev/null
-		# We have the backup's filepath.
-		# Obtain the $RESTORE_PATH by stripping off ".bak$postfix".
-		string match -q -r "(?<RESTORE_PATH>.*).bak$postfix" $FILEPATH
+	# We'll determine the path to back up -from and -to based on the arguments
+	# provided, and then set these variables accordingly.
+	set -l restore_from
+	set -l restore_to
 
-		if test -z $RESTORE_PATH
-			echo "Failed to deduce RESTORE_PATH by stripping off .bak$postfix"
+	if test -n "$postfix"
+
+		# We were given $postfix, so use it as a postfix.
+		# Include a special case of being given 0, in which case we just use the
+		# .bak postfix
+		if test $postfix = '0'
+			set restore_from "$filepath.bak"
+		else
+			set restore_from "$filepath.bak.$postfix"
+		end
+		set restore_to "$filepath"
+
+	else if string match -q -r '(?<restore_to>.*)\.bak(\..+)?$' $filepath > /dev/null
+		# Our filepath matches a backup file filepath and the above has set
+		# $restore_to, so just set $restore_from to be $filepath.
+		set restore_from "$filepath"
+
+	else
+		# We've been given neither a postfix nor a backup-from filepath.
+		# Assume we've been given a backup-to filepath and find a backup-from
+		# filepath.
+		set restore_to "$filepath"
+
+		for i in (seq 100 -1 1)
+			if test -e "$filepath.bak.$i"
+				set restore_from "$filepath.bak.$i"
+				break
+			end
 		end
 
-		rm -rf $RESTORE_PATH
-		cp -r $FILEPATH $RESTORE_PATH
-
-	else
-		# We have the file's filepath, maybe with a postfix number.
-		rm -rf $FILEPATH
-		cp -r "$FILEPATH.bak$postfix" "$FILEPATH"
+		# If we haven't found a backup with a numeric postfix, 'guess' a standard
+		# .bak postfix.
+		if test -z "$restore_from"
+			set restore_from "$filepath.bak"
+		end
 	end
+
+	# $restore_from and $restore_to should now be set.
+	# Ensure that they are and do final checks on them.
+
+	if test -z "$restore_to"
+		usage 'Failed to determine filepath to restore to' && return 1
+	end
+	if ! test -e "$restore_from"
+		usage "Backup file $restore_from does not exist" && return 1
+	end
+
+	# If we'd be overwriting another file/directory, get user confirmation.
+	if test -e "$restore_to"
+
+		echo "This will overwrite '$restore_to' with '$restore_from'."
+		echo ''
+		read -l -P "Are you sure? [y/N] " confirm
+
+		switch (string lower $confirm)
+			case y ye yes
+				# Continue.
+			case '*'
+				echo 'Cancelling...'
+				return 1
+		end
+
+		rm -rf "$restore_to"
+	end
+
+	# Finally perform the restoration.
+	cp -r "$restore_from" "$restore_to"
 end
+
+complete -c unbak -s h -l help -d 'Print the usage string and exit'
