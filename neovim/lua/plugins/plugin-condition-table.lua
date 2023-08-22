1cde7c302e50cbd3b920c3e2ad98dcf7eda3c6bd
Alex W <alexander.stephen.wall@gmail.com>
2023-08-22 02:32:24 +0100

    Disabled nvim-ts-rainbow2 due to big errors on loading C++ files

--- neovim/lua/plugins/plugin-condition-table.lua
+++ neovim/lua/plugins/plugin-condition-table.lua
@@ -4,7 +4,7 @@ M.enable_plugin_table = {
 	['indent-blankline']     = true,
 	['markdown-preview']     = false,
 	['neoscroll']            = true,
-	['nvim-ts-rainbow']      = true,
+	['nvim-ts-rainbow']      = false,
 	['telescope-fzf-native'] = true,
 	['ufo']                  = false
 }
